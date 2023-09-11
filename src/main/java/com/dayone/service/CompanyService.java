e5f5445febd360bade10d5f5ca64ca3076f48764
KimMunjin <reminisce057@gmail.com>
2023-09-11 18:58:09 +0900

    First Commit

---
+++ src/main/java/com/dayone/service/CompanyService.java
@@ -0,0 +1,85 @@
+package com.dayone.service;
+
+import com.dayone.exception.impl.AlreadyExistCompany;
+import com.dayone.exception.impl.NoCompanyException;
+import com.dayone.exception.impl.NoTickerException;
+import com.dayone.model.Company;
+import com.dayone.model.ScrapedResult;
+import com.dayone.persist.CompanyRepository;
+import com.dayone.persist.DividendRepository;
+import com.dayone.persist.entity.CompanyEntity;
+import com.dayone.persist.entity.DividendEntity;
+import com.dayone.scraper.Scraper;
+import lombok.AllArgsConstructor;
+import org.apache.commons.collections4.Trie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CompanyService {
+
+    private final Trie trie;
+    private final Scraper yahooFinanceScraper;
+
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+
+    public Company save(String ticker) {
+        boolean exists = this.companyRepository.existsByTicker(ticker);
+        if (exists) {
+            throw new AlreadyExistCompany();
+        }
+        return this.storeCompanyAndDividend(ticker);
+    }
+
+    public Page<CompanyEntity> getAllCompany(Pageable pageable) {
+        return this.companyRepository.findAll(pageable);
+    }
+
+    private Company storeCompanyAndDividend(String ticker) {
+        Company company = this.yahooFinanceScraper.scrapCompanyByTicker(ticker);
+        if (ObjectUtils.isEmpty(company)) {
+            throw new NoTickerException();
+        }
+
+        ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(company);
+
+        CompanyEntity companyEntity = this.companyRepository.save(new CompanyEntity(company));
+        List<DividendEntity> dividendEntityList = scrapedResult.getDividends().stream()
+                .map(e -> new DividendEntity(companyEntity.getId(), e))
+                .collect(Collectors.toList());
+        this.dividendRepository.saveAll(dividendEntityList);
+        return company;
+    }
+
+    public void addAutocompleteKeyword(String keyword) {
+        this.trie.put(keyword, null);
+    }
+
+    public List<String> autocomplete(String keyword) {
+        return (List<String>) this.trie.prefixMap(keyword).keySet()
+                .stream()
+                .limit(10)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteAutocompleteKeyword(String keyword) {
+        this.trie.remove(keyword);
+    }
+
+    public String deleteCompany(String ticker) {
+        var company = this.companyRepository.findByTicker(ticker)
+                .orElseThrow(() -> new NoCompanyException());
+        this.dividendRepository.deleteAllByCompanyId(company.getId());
+        this.companyRepository.delete(company);
+
+        this.deleteAutocompleteKeyword(company.getName());
+        return company.getName();
+    }
+}
